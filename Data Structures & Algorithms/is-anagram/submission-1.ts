67b252932a2776a8be63c093f2baa0896a4af5fb
DRT1000 <77331808+DRT1000@users.noreply.github.com>
2026-08-02 10:31:55 +0200

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.ts
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        const len = s.length;
+
+        if(len !== t.length) {
+            return false;
+        }
+
+        // const counts = new Array(26).fill(0);
+
+        // for(let i = 0; i <= len - 1; i++) {
+        //     counts[s[i].charCodeAt(0) - 97]++;
+        //     counts[t[i].charCodeAt(0) - 97]--;
+        // }
+
+        // return counts.every(el => !el)
+
+        const map = new Map<string, number>();
+
+        for(let char of s) {
+            map.set(char, (map.get(char) ?? 0) + 1)
+        }
+
+        for(let char of t) {
+            const setted = map.get(char);
+            if (!setted) {
+                return false;
+            }
+            map.set(char, setted - 1);
+        }
+
+        return true
+    }
+}
